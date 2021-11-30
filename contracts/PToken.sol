a3e90555f7ff34ea49493aa362ddd9fb4e1febb1
Mikołaj Dudzic <mikolajdudzic@gmail.com>
2021-11-30 12:36:55 +0100

    pToken minting contract

--- contracts/PToken.sol
+++ contracts/PToken.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+//@notice minting pToken for test purposes only
 contract PToken is ERC20 {
 	constructor() ERC20("Payment Token", "PToken") {
 		_mint(msg.sender, 1000000000000000000000000);
